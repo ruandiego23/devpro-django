da4680d761671440933469e530dd5e94269edf5e
Diego <ruan.diego.anjos@outlook.com>
2024-12-08 00:39:40 -0300

    trying new workflows

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("Hello, world. You're at the polls page.")
+
+
+def debug(request):
+    return JsonResponse(request.headers)
